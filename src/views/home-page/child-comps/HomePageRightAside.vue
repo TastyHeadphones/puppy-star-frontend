e6676da64dcc13957c0370bf474aeb4332ccdaa1
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2021-08-21 23:39:09 +0800

    增加侧边栏

--- src/views/home-page/child-comps/HomePageRightAside.vue
+++ src/views/home-page/child-comps/HomePageRightAside.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>右边的侧边栏</div>
+  <div class = "app">
+    <div class="title">🐱宠物动态</div>
+    <hr />
+    <div class="video">
+      <iframe src="//player.bilibili.com/player.html?aid=286028867&bvid=BV1vf4y1y7rs&cid=205094794&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+      <iframe src="//player.bilibili.com/player.html?aid=462048204&bvid=BV1sL411n7jT&cid=380905781&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+      <iframe src="//player.bilibili.com/player.html?aid=413261518&bvid=BV1GV411C7hf&cid=194545066&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+      <iframe src="//player.bilibili.com/player.html?aid=889278835&bvid=BV1HP4y1471G&cid=375171774&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+    </div>
+  </div>
+
+
 </template>
 
 <script>
+
+
+
 export default {
-  name: "HomePageRigthAside",
+  name: "HomePageRightAside",
 };
 </script>
 
 <style lang="scss" scoped>
+.title{
+  font-size: large;
+  text-align: center;
+  color: #035f5f;
+}
+.video {
+  text-align: center;
+}
+
 </style>
